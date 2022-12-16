c90de21828a1a23dc8b46e29908345f0e0ab0d32
Chalos77 <103358913+Chalos77@users.noreply.github.com>
2022-12-16 19:47:46 +0100

    Update Annexe - Preprocessing.py

--- Annexe - Preprocessing.py
+++ Annexe - Preprocessing.py
@@ -11,7 +11,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler, RobustScaler,MinMaxScaler
 from xgboost import XGBRegressor
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
