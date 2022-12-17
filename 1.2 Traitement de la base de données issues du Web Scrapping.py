451f19f4ea1a30c37e0dc769c722ac7cf7bba6e8
Chalos77 <103358913+Chalos77@users.noreply.github.com>
2022-12-17 14:21:28 +0100

    Update 1.2 Traitement de la base de données issues du Web Scrapping.py

--- 1.2 Traitement de la base de données issues du Web Scrapping.py
+++ 1.2 Traitement de la base de données issues du Web Scrapping.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 29 08:45:57 2022
+Created on Sat Dec 17 13:01:45 2022
 
 @author: charlesrollet
 """
@@ -12,17 +12,17 @@ from geopy.geocoders import Nominatim
 import geopandas
 from shapely.geometry import Point
 
-
-df = pd.read_csv("/Users/charlesrollet/Desktop/Cadrillage.csv",\
-                 names=['numéro_du_bien', 'adresse_du_bien', 'prix_du_bien', 'date_de_vente_du_bien', 'détails_du_bien'],\
+# On lit la base issue du web scraping en renommant de la même manière les colonnes que celles issues de la base du gouv
+df = pd.read_csv("/Users/charlesrollet/Desktop/TestWS1.csv",\
+                 names=['adresse_du_bien', 'type_local', 'valeur_fonciere', 'prix_m2', 'details'],\
                   index_col=0)
 indexNames = df[ df['adresse_du_bien'] == 'addresse'].index
 df.drop(indexNames , inplace=True)
-#df.drop(index =2235, inplace=True)
+
+# On supprime les doublons possibles issus du web scraping
 df.drop_duplicates(keep = 'first', inplace=True)
 
-# Reconfiguration des données brutes du Dataframe
-
+# On crée une fonction afin de créer des colonnes à partir des anciennes
 def func(x):
     if x[0]=='s':
         a=x[1:]
@@ -36,63 +36,51 @@ def separator(c):
 def maxi_separator(c, d):
     return lambda x: pd.Series(str(x).split(c, d))
 
+# On s'occupe d'abord de nettoyer les colonnes valeur_fonciere et prix_m2 en supprimant les chaînes de caractères 
+df['prix_m2']=df['prix_m2'].str.split('€/m²').str[0]
+df['prix_m2']=df['prix_m2'].str.replace(' ', '')
+df['valeur_fonciere']=df['valeur_fonciere'].str.split('€').str[0] 
+df['valeur_fonciere']=df['valeur_fonciere'].str.replace(' ', '')
 
-list_column = df.columns.tolist()
+# On crée deux colonnes à partir de adresse_du_bien qui contient l'adresse brute et la ville
+df[['adresse', 'ville']]=df.adresse_du_bien.str.split(' - ', expand=True)
 
+# On supprime toutes les données qui ne sont pas localisées à Paris issues du cadrillage rectangulaire du web scraping
+indexNames = df[df['ville'] != 'PARIS'].index
+df.drop(indexNames , inplace=True)
 
-#Création des nouvelles colonnes pertinentes
-df['adresse globale'] = \
-    df['adresse_du_bien'].str.replace('\n', ' ')
-df[['adresse precise','arrondissement']] = \
-    df.adresse_du_bien.apply(separator('\n'))
-df[['prix de vente','prix du m2']] = \
-    df.prix_du_bien.apply(separator('\n')) 
-df[['date de vente','colonne']] = \
-    df.date_de_vente_du_bien.apply(separator('\n')) 
-df[['type de bien','details_bis']] = \
-    df.détails_du_bien.apply(maxi_separator(' ',1)) 
-df[['nombre de pièces','surface_du_bien']] = \
-    df.details_bis.apply(separator('pièce')) 
-df[['surface du bien en m2','poubelle']] = \
-    df.surface_du_bien.apply(maxi_separator('m', 1))
-#Suppression des anciennes colonnes et des doublons
-df.drop(list_column, axis=1, inplace=True)
-df.drop(['colonne', 'surface_du_bien', 'poubelle', 'details_bis'], axis=1, inplace=True)
-df.drop_duplicates()
-    
-#Nettoyage des données pour pouvoir les manipuler
-df['date de vente']=df['date de vente'].str[9:]
-df['arrondissement']=df['arrondissement'].str.split(' PARIS').str[0]
-df['prix du m2']=df['prix du m2'].str.split('€/m²').str[0] 
-df['prix de vente']=df['prix de vente'].str.split('€').str[0] 
-df['prix de vente']=df['prix de vente'].str.replace(' ', '')
-df['prix du m2']=df['prix du m2'].str.replace(' ', '')
-df['surface du bien en m2']= df['surface du bien en m2'].apply(func)
-#encodage binaire pour type de bien
-df['type de bien']=df['type de bien'].str.replace('Appartement','0')
-df['type de bien']=df['type de bien'].str.replace('Maison','1')
-#Convertion des variables quantitatives en entiers
-list_newcolumn = list(df)
-list_newcolumn.remove('date de vente')
-list_newcolumn.remove('adresse precise')
-list_newcolumn.remove('adresse globale')
+# On nettoye la colonne details pour contruire les colonnes nombre_pieces_principales, surface_reelle_bati et date_mutation
+df['details']=df['details'].str.replace('\n', ' ')
+df[['nombre_pieces_principales','poubelle']] = df.details.apply(maxi_separator('Surface', 1))
+df[['surface_reelle_bati','poubelle']] = df.poubelle.apply(maxi_separator('m²', 1))
+df[['poubelle','date_mutation']] = df.poubelle.apply(maxi_separator('le', 1))
+df[['poubelle','nombre_pieces_principales']] = df.nombre_pieces_principales.apply(maxi_separator('Pièces', 1))
+df.drop(['poubelle', 'details'], axis=1, inplace=True)
+
+# On convertit les chaines de caratères numériques en nombres entiers
+list_newcolumn = ['valeur_fonciere','prix_m2','nombre_pieces_principales','surface_reelle_bati']
 for column in list_newcolumn:
     df[column]=df[column].astype(int)
-print(df)
+    
+# On supprime les données qui semblent de mauvaises qualités par rapport à leur prix au m2
+indexNames = df[(df['prix_m2']< 9000)].index
+df.drop(indexNames , inplace=True)
+indexNames1 = df[(df['prix_m2']> 13000)].index
+df.drop(indexNames1 , inplace=True)
 
-n = len(df.index)
+# On convertit les adresses des biens en données géographiques
+geolocator = Nominatim(timeout=5, user_agent = "myGeolocator")
+df['coordinates'] = df['adresse_du_bien'].apply(geolocator.geocode)
+df = df.dropna() #enlève les lignes avec des 'Nan'
+df.reset_index(drop=True) #censer renuméroter le DataFrame mais ça marche pas mdrr
+df['longitude'] = df['coordinates'].apply(lambda x: x.point.longitude)
+df['latitude'] = df['coordinates'].apply(lambda x: x.point.latitude)
 
-#geolocator = Nominatim(timeout=5, user_agent = "myGeolocator")
-#df['coordinates'] = df['adresse globale'].apply(geolocator.geocode)
-#df = df.dropna() #enlève les lignes avec des 'Nan'
-#df.reset_index(drop=True) #censer renuméroter le DataFrame mais ça marche pas mdrr
-#df['longitude'] = df['coordinates'].apply(lambda x: x.point.longitude)
-#df['latitude'] = df['coordinates'].apply(lambda x: x.point.latitude)
-#df.drop(['coordinates'], axis=1, inplace=True)
-#print(df['longitude'], df['latitude'], n, len(df.index))
+# On supprime les colonnes qui ne sont plus utiles et réordonne celles utiles afin que la base soit similaire à celle du gouv nettoyée 
+df.drop(['coordinates', 'adresse_du_bien', 'adresse', 'ville', 'prix_m2'], axis=1, inplace=True)
+df = df[['date_mutation','valeur_fonciere','type_local','surface_reelle_bati','nombre_pieces_principales','longitude','latitude']]
 
-
-df.to_csv('Clean data.csv')
+df.to_csv('Clean_WS.csv')
 
 
 
