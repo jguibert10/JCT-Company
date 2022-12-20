b0cea3dfa98a7e67e82c3a3aaa1697cc42928802
Chalos77 <103358913+Chalos77@users.noreply.github.com>
2022-12-21 00:04:20 +0100

    Update Algorithme d'entrainement du model.py

--- 5. Modélisation/Algorithme d'entrainement du model.py
+++ 5. Modélisation/Algorithme d'entrainement du model.py
@@ -20,7 +20,8 @@ from sklearn.linear_model import LogisticRegression, LinearRegression
 # Model Selection and metrics
 from sklearn.model_selection import cross_val_score, GridSearchCV, StratifiedKFold
 from sklearn.pipeline import make_pipeline
-from statistics import mean
+from sklearn.metrics import confusion_matrix, r2_score, mean_absolute_error, mean_squared_error,mean_squared_log_error
+
 #import shap  
 
 X_train = pd.read_excel('/Users/charlesrollet/Desktop/X_train_rob.xls')
@@ -51,7 +52,7 @@ model_knn = make_pipeline(KNeighborsRegressor())
 model_svm = make_pipeline(SVC())
 
 # On souhaite que le recall et la precision soient les scores qui comptent le plus
-SCORING = 'f1_weighted' 
+SCORING = 'r2' 
 
 # On crée une fonction qui donne les meilleurs hyperparamètres de chaque modèle
 def best_param(X, Y, pipeline, dico_param):
@@ -60,7 +61,7 @@ def best_param(X, Y, pipeline, dico_param):
     grid = GridSearchCV(
         pipeline, 
         param_grid=dico_param,  
-        cv=5, 
+        cv=3, 
         refit=True,
         scoring=SCORING)
     
@@ -80,23 +81,52 @@ best_model_svm = best_param(X_train, Y_train, model_svm, dico_param_svm)
 best_model_rf = best_param(X_train, Y_train, model_rf, dico_param_rf)
 
 # On crée une fonction pour tester le meilleur modèle et obtenir différents résultats sur ses performances 
-def fit(X_train, X_test, Y_train, model):
+def fit(mettre_X_train, mettre_X_test, mettre_Y_train, model):
     
-    accuracy = mean(cross_val_score(model, X_train, Y_train, cv=(), scoring='accuracy'))
-    auc = mean(cross_val_score(model, X_train, Y_train, cv=StratifiedKFold(), scoring='roc_auc'))
-    precision = mean(cross_val_score(model, X_train, Y_train, cv=StratifiedKFold(), scoring='precision'))
-    recall = mean(cross_val_score(model, X_train, Y_train, cv=StratifiedKFold(), scoring='recall'))
-    f1_weighted = mean(cross_val_score(model, X_train, Y_train, cv=StratifiedKFold(), scoring='f1_weighted'))
-    proba = model.predict_proba(X_test)[:,1]
+    mean_absolute_error = mean(cross_val_score(model, mettre_X_train, mettre_Y_train, cv=StratifiedKFold(), scoring='neg_mean_absolute_error'))
+    mean_squared_error = mean(cross_val_score(model, mettre_X_train, mettre_Y_train, cv=StratifiedKFold(), scoring='neg_mean_squared_error'))
+    max_error = mean(cross_val_score(model, mettre_X_train, mettre_Y_train, cv=StratifiedKFold(), scoring='max_error'))
+    explained_variance =mean(cross_val_score(model, mettre_X_train, mettre_Y_train, cv=StratifiedKFold(), scoring='explained_variance'))
+    mean_poisson_deviance = mean(cross_val_score(model, mettre_X_train, mettre_Y_train, cv=StratifiedKFold(), scoring='neg_mean_poisson_deviance'))
+
+    #print('r2_score: {:.3g}'.format(r2_score))
+    print('mean_absolute_error: {:.3g}'.format(mean_absolute_error))
+    print('mean_squared_error: {:.3g}'.format(mean_squared_error))
+    print('max_error: {:.3g}'.format(max_error))
+    print('explained_variance: {:.3g}'.format(explained_variance))
+    print('mean_poisson_deviance: {:.3g}'.format(mean_poisson_deviance))
     
-    print('Accuracy: {:.3g}'.format(accuracy))
-    print('Precision: {:.3g}'.format(precision))
-    print('Recall: {:.3g}'.format(recall))
-    print('F1 weighted: {:.3g}'.format(f1_weighted))
-    print('AUC score: {:.3g}'.format(auc))
-    
-    return accuracy, auc, precision, f1_weighted, proba, recall
+    return mean_absolute_error, mean_squared_error, max_error, explained_variance, mean_poisson_deviance
 
 # On obtient différents scores pour le modèle qui obtient la meilleur précision suite à best_paraù
-accuracy_log, auc_log, precision_log, f1_weighted_log, proba_log, recall_log = fit(X_train, X_test, Y_train, best_model_log)
+mean_absolute_error_log, mean_squared_error_log, max_error_log, explained_variance_log, mean_poisson_deviance_log = fit(X_train_st, X_test_st, Y_train, best_model_log)
+mean_absolute_error_tree, mean_squared_error_tree, max_error_tree, explained_variance_tree, mean_poisson_deviance_tree = fit(X_train_mm, X_test_mm, Y_train, best_model_tree)
+mean_absolute_error_knn, mean_squared_error_knn, max_error_knn, explained_variance_knn, mean_poisson_deviance_knn = fit(X_train_rob, X_test_rob, Y_train, best_model_knn)
+mean_absolute_error_svm, mean_squared_error_svm, max_error_svm, explained_variance_svm, mean_poisson_deviance_svm = fit(X_train_st, X_test_st, Y_train, best_model_svm)
+mean_absolute_error_rf, mean_squared_error_rf, max_error_rf, explained_variance_rf, mean_poisson_deviance_rf = fit(X_train_st, X_test_st, Y_train, best_model_rf)
+mean_absolute_error_lr, mean_squared_error_lr, max_error_lr, explained_variance_lr, mean_poisson_deviance_lr = fit(X_train_st, X_test_st, Y_train, model_lr)
 
+# On crée une fonction qui renvoie les meilleurs features 
+def best_features(model, X_train_df, seuil) :     
+    features_importances = []
+    index_best_features = []
+    list_results = model.feature_importances_.tolist()
+    best_features = []
+    for i in range(len(list_results)):
+        if list_results[i] > seuil :
+            index_best_features.append(i)
+            features_importances.append(list_results[i])
+            best_features.append(X_train_df.columns[i])
+    return best_features, features_importances
+
+# Pour le modèle RandomForest Regressor on regarde les features les plus importantes 
+mean_absolute_error, mean_squared_error, max_error, explained_variance, mean_poisson_deviance = fit(X_train_st, X_test_st, Y_train, best_model_rf)
+
+best_features_rf, features_importance_rf = best_features(best_model_rf, X_train_st, 0.01)
+
+pd.DataFrame(features_importance_rf, index = best_features_rf).plot.bar(color='purple')
+plt.suptitle('Feature importances pour RandomForest', fontsize=15)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+plt.legend(fontsize=10)
+plt.show()
